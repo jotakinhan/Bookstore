0eccb64c8ccde1904c4c85238df93739d772ecdc
unknown <jotakinhan@gmail.com>
2018-03-07 00:02:57 +0200

    category

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -1,15 +1,72 @@
 package com.example.Bookstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Bookstore.model.Book;
+import com.example.Bookstore.model.BookRepository;
+import com.example.Bookstore.model.CategoryRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository crepository;
+	
 	@RequestMapping("/index")
-	public String index() {
-		return "This is the main page";
+	public String index(Model model) {
+		return "index";
 	}
+	
+	@RequestMapping(value = "/booklist")
+    public String bookList(Model model) {	
+		List<Book> books = repository.findAll();
+		model.addAttribute("books", books);
+        return "booklist";
+    }
+  
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+    	model.addAttribute("categories", crepository.findAll());
+        return "add";
+    }
+    
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+    	Book book = repository.findOne(bookId);
+    	model.addAttribute("book", book);
+    	model.addAttribute("categories", crepository.findAll());
+    	
+        return "edit";
+    }
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }   
+	
+	@RequestMapping(value = "/saveid/{id}", method = RequestMethod.POST)
+    public String saveId(@PathVariable("id") Long bookId, Model model) {
+		Book book = repository.findOne(bookId);
+        repository.save(book);
+        return "redirect:../booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.delete(bookId);
+        return "redirect:../booklist";
+    }
 	
 }
--- src/main/java/com/example/Bookstore/model/Book.java
+++ src/main/java/com/example/Bookstore/model/Book.java
@@ -1,9 +1,110 @@
 package com.example.Bookstore.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Book {
-String title;
-String author;
-int year;
-String isbn;
-double price;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
+	
+	@ManyToOne
+    @JoinColumn(name = "categoryid")
+    private Category category;
+
+	public Book() {
+		super();
+		this.title = null;
+		this.author = null;
+		this.year = 0;
+		this.isbn = null;
+		this.price = 0;
+		this.category = null;
+	}
+
+	public Book(String title, String author, int year, String isbn, double price, Category category) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		if (this.category != null)
+			return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + ", category=" + this.getCategory() + "]";
+		else
+			return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+					+ ", price=" + price + "]";
+	}
 }
